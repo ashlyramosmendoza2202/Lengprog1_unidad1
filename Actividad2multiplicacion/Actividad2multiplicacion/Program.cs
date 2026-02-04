13d3313b5b1d6a98a2913159707eff8cdfeabb54
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 03:21:05 -0400

    Ejercicio3multiplicacion

---
+++ Actividad2multiplicacion/Actividad2multiplicacion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class ProgramaResta
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("---------------- Operacion de Resta ----------------");
+        Console.WriteLine(" ");
+
+        int valorA;
+        int valorB;
+        int resultado;
+
+        Console.WriteLine("Ingrese el primer numero entero: ");
+        valorA = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero entero: ");
+        valorB = Convert.ToInt32(Console.ReadLine());
+
+       
+        resultado = valorA - valorB;
+
+    
+        Console.WriteLine(" ");
+        Console.WriteLine("El resultado de restar " + valorA + " menos " + valorB + " es: " + resultado);
+
+  
+        Console.ReadKey();
+    }
+}
