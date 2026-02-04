3b77e9426f061729c7f15535c6590df397787448
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 02:24:04 -0400

    Actividad 1 suma.

--- Primerprograma1/Primerprograma1/Program.cs
+++ Primerprograma1/Primerprograma1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num1, num2, suma;
+
+    cout << "Introduce El Primer umero: ";
+    cin >> num1;
+
+    cout << "Introduce el segundo numero: ";
+    cin >> num2;
+
+    suma = num1 + num2;
+
+    cout << "La suma es: " << suma << endl;
+
+    return 0;
+}
