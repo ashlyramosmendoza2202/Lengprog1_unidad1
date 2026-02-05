2c77e103a7418b3920f1cdeaec7bdaed3b67415f
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 21:32:00 -0400

    cargando

---
+++ Ejercicio5nombreyedad/Ejercicio5nombreyedad/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Ejercicio5Edad
+{
+    static void Main()
+    {
+        Console.WriteLine("_________ Programa de Verificacion de Edad _________");
+        Console.WriteLine(" ");
+
+        string Nombre;
+        int Edad;
+
+        Console.WriteLine("Introduce tu nombre: ");
+        Nombre = Console.ReadLine();
+
+        Console.WriteLine("Introduce tu edad: ");
+        Edad = Convert.ToInt32(Console.ReadLine());
+
+        if (Edad >= 18)
+        {
+            Console.WriteLine(Nombre + " es adulto.");
+        }
+        else
+        {
+            Console.WriteLine(Nombre + " es menor de edad.");
+        }
+    }
+}
