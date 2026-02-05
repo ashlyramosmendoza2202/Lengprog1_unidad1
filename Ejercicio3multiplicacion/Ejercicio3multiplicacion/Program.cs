3ae433d604adf6e9d5ad8b37d7438d30178c91b2
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 20:55:50 -0400

    cargando

---
+++ Ejercicio3multiplicacion/Ejercicio3multiplicacion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Ejercicio3Multiplicacion
+{
+    static void Main()
+    {
+        Console.WriteLine("_________ Este es un Programa que Multiplica dos numeros _________");
+        Console.WriteLine(" ");
+
+        int PrimerValor;
+        int SegundoValor;
+        int Resultado;
+
+        Console.WriteLine("Introduce el primer valor: ");
+        PrimerValor = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce tu segundo valor: ");
+        SegundoValor = Convert.ToInt32(Console.ReadLine());
+
+        Resultado = PrimerValor * SegundoValor;
+
+        Console.WriteLine("La multiplicacion de " + PrimerValor + " * " + SegundoValor + " es " + Resultado);
+    }
+}
