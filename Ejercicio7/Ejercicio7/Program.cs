901330c809994737e250cf8d9f4334f6c6e393ba
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 22:14:26 -0400

    cargando

---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Ejercicio7Impares
+{
+    static void Main()
+    {
+        Console.WriteLine("Numeros Impares del 1 al 100:");
+
+        int i = 1;
+
+        while (i <= 100)
+        {
+            Console.WriteLine(i);
+            i = i + 2;
+        }
+    }
+}
