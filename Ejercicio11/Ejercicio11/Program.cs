342ff6f6db4f8db4a7bff8695b6e05be3bdeb091
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 23:06:46 -0400

    cargando

---
+++ Ejercicio11/Ejercicio11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Ejercicio11
+{
+    static void Main()
+    {
+        Console.WriteLine("Escribe un numero:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int contador = 0;
+        int i = 1;
+
+        while (i <= num)
+        {
+            if (num % i == 0)
+            {
+                contador = contador + 1;
+            }
+            i = i + 1;
+        }
+
+        if (contador == 2)
+        {
+            Console.WriteLine("Es primo");
+        }
+        else
+        {
+            Console.WriteLine("No es primo");
+        }
+    }
+}
