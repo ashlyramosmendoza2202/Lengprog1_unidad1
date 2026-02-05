7bc8dc96d043e94ef6ee6b131be666ee88d69ff7
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 21:01:01 -0400

    cargando

---
+++ Ejercicio4division/Ejercicio4division/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Ejercicio4Division
+{
+    static void Main()
+    {
+        Console.WriteLine("_________ Este es un Programa que Divide dos numeros _________");
+        Console.WriteLine(" ");
+
+        double PrimerValor;
+        double SegundoValor;
+        double Resultado;
+
+        Console.WriteLine("Introduce el dividendo: ");
+        PrimerValor = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Introduce el divisor: ");
+        SegundoValor = Convert.ToDouble(Console.ReadLine());
+
+        Resultado = PrimerValor / SegundoValor;
+
+        Console.WriteLine("La division de " + PrimerValor + " / " + SegundoValor + " es " + Resultado);
+    }
+}
