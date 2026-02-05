e1dde6da9a913ce10a75c66c9a8d4750af6f1aae
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 22:34:02 -0400

    cargando

---
+++ Ejercicio8/Ejercicio8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Ejercicio8
+{
+    static void Main()
+    {
+        Console.WriteLine("Pon un numero:");
+
+        int n;
+        n = Convert.ToInt32(Console.ReadLine());
+
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("Es par");
+        }
+        else
+        {
+            Console.WriteLine("Es impar");
+        }
+    }
+}
