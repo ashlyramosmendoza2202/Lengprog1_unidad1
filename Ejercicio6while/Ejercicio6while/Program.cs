150b9e1f47a3f811f68ce9b58e959b4f589227d5
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-04 22:04:57 -0400

    cargando

---
+++ Ejercicio6while/Ejercicio6while/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Ejercicio6Pares
+{
+    static void Main()
+    {
+        Console.WriteLine("Numeros Pares del 1 al 100:");
+
+        int i = 2;
+
+        while (i <= 100)
+        {
+            Console.WriteLine(i);
+            i = i + 2;
+        }
+    }
+}
