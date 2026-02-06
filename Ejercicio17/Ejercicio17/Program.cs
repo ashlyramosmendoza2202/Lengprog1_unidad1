031cef25bc5f964b852773706415fa64e0ba5e6c
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 23:25:19 -0400

    cargando

---
+++ Ejercicio17/Ejercicio17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ejercicio17
+{
+    static void Main()
+    {
+        Console.WriteLine("Escribe un numero:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int suma = 0;
+
+        while (n > 0)
+        {
+            suma = suma + (n % 10);
+            n = n / 10;
+        }
+
+        Console.WriteLine("La suma de sus digitos es: " + suma);
+    }
+}
