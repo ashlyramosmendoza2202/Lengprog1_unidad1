4fc82cd552228b53349a82cdbfe4192bde4ad52f
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-06 19:23:49 -0400

    cargando

---
+++ Ejercicio22/Ejercicio22/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Ejercicio22
+{
+    static void Main()
+    {
+        Console.WriteLine("Año:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 4 == 0) { Console.WriteLine("Si es bisiesto"); }
+        else { Console.WriteLine("No es bisiesto"); }
+    }
+}
