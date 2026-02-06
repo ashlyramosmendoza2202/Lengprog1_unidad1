2575f1c2d5ae3d702357b3e714ee2c8f73ac870a
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 23:16:00 -0400

    cargando

---
+++ Ejercicio15/Ejercicio15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ejercicio15
+{
+    static void Main()
+    {
+        Console.WriteLine("Dime un numero:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        long f = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            f = f * i;
+        }
+
+        Console.WriteLine("El factorial es: " + f);
+    }
+}
