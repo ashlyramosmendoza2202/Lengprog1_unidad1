75696a9ab095629f7ddbc15ce67a88e4c9adf2b7
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 23:21:23 -0400

    cargando

---
+++ Ejercicio16/Ejercicio16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Ejercicio16
+{
+    static void Main()
+    {
+        Console.WriteLine("Escribe un numero:");
+        string texto = Console.ReadLine();
+        string alReves = "";
+
+        for (int i = texto.Length - 1; i >= 0; i--)
+        {
+            alReves = alReves + texto[i];
+        }
+
+        if (texto == alReves)
+        {
+            Console.WriteLine("Es capicua");
+        }
+        else
+        {
+            Console.WriteLine("No es capicua");
+        }
+    }
+}
