cb67e34eac80165c35ab2f795bc1786930367741
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 23:41:26 -0400

    cargando

---
+++ Ejercicio19/Ejercicio19/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+class Ejercicio19
+{
+    static void Main()
+    {
+        Console.WriteLine("Cuantos numeros quieres?");
+        int limite = Convert.ToInt32(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+        int i = 1;
+
+        while (i <= limite)
+        {
+            Console.WriteLine(a);
+
+            int suma = a + b;
+            a = b;
+            b = suma;
+
+            i = i + 1;
+        }
+    }
+}
+
+
