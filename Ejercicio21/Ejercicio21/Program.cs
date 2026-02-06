744db5654c6f7c8f3656cfb6757cff73acdb6243
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-06 01:16:31 -0400

    cargando

---
+++ Ejercicio21/Ejercicio21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Ejercicio21
+{
+    static void Main()
+    {
+        Console.WriteLine("Cuantos numeros son?");
+        int total = Convert.ToInt32(Console.ReadLine());
+        double suma = 0;
+        int i = 1;
+        while (i <= total)
+        {
+            Console.WriteLine("Numero " + i + ":");
+            suma = suma + Convert.ToDouble(Console.ReadLine());
+            i = i + 1;
+        }
+        Console.WriteLine("Promedio: " + (suma / total));
+    }
+}
