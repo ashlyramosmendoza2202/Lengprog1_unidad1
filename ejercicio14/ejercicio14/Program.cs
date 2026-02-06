2dbc849041abea4d8bc389a52908197b60ded9d6
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 23:07:19 -0400

    cargando

---
+++ ejercicio14/ejercicio14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Ejercicio14
+{
+    static void Main()
+    {
+        Console.WriteLine("Inicio:");
+        int inicio = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Final:");
+        int final = Convert.ToInt32(Console.ReadLine());
+
+        int suma = 0;
+
+        for (int i = inicio; i <= final; i++)
+        {
+            suma = suma + i;
+        }
+
+        Console.WriteLine("La suma es: " + suma);
+    }
+}
