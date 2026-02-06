998068e1b5aae360c0c2e97f84b0a7cec27abde6
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 22:35:05 -0400

    cargando

---
+++ Ejercicio13/Ejercicio13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Ejercicio13
+{
+    static void Main()
+    {
+        Console.WriteLine("Inicio:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Final:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int i = a;
+        while (i <= b)
+        {
+            Console.WriteLine(i);
+            i = i + 1;
+        }
+    
