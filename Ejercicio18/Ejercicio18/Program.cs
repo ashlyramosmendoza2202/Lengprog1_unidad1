a50f90eb041b90268ae97652efd5036c2a1ae641
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 23:28:43 -0400

    cargando

---
+++ Ejercicio18/Ejercicio18/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Ejercicio18
+{
+    static void Main()
+    {
+        Console.WriteLine("Numero 1:");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Numero 2:");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Numero 3:");
+        int n3 = Convert.ToInt32(Console.ReadLine());
+
+        if (n1 > n2  n1 > n3)
+        {
+            Console.WriteLine("El mayor es: " + n1);
+        }
+        else if (n2 > n3)
+        {
+            Console.WriteLine("El mayor es: " + n2);
+        }
+        else
+        {
+            Console.WriteLine("El mayor es: " + n3);
+        }
+    }
+}
