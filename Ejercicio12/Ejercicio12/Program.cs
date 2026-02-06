b9a36240380656855eca497807ecd134a68d89a0
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 22:10:02 -0400

    cargando

---
+++ Ejercicio12/Ejercicio12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Ejercicio12
+{
+    static void Main()
+    {
+        Console.WriteLine("Escribe un numero:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int contador = 0;
+        int i = 1;
+
+        while (i <= num)
+        {
+            if (num % i == 0)
+            {
+                contador = contador + 1;
+            }
+            i = i + 1; 
+        }
+
+        if (contador == 2)
+        {
+            Console.WriteLine("Es perfecto");
+        }
+        else
+        {
+            Console.WriteLine("No es perfecto");
+        }
+    }
+}
