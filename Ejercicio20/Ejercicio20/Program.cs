74f89b382377dfa558b985f377749fba8bffd087
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-05 23:45:37 -0400

    cargando

---
+++ Ejercicio20/Ejercicio20/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Ejercicio20
+{
+    static void Main()
+    {
+        Console.WriteLine("Numero:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+        while (i <= 10)
+        {
+            Console.WriteLine(n + "x" + i + "=" + (n * i));
+            i = i + 1;
+        }
+    }
+}
+
+
