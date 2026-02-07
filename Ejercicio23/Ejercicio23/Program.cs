921925ba549f1548e321176750cb758678a34b10
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-06 21:23:08 -0400

    cargando

---
+++ Ejercicio23/Ejercicio23/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Ejercicio16
+{
+    static void Main()
+    {
+        Console.WriteLine("Escribe algo:");
+        string t = Console.ReadLine();
+        string res = "";
+        int i = t.Length - 1;
+        while (i >= 0)
+        {
+            res = res + t[i];
+            i = i - 1;
+        }
+        Console.WriteLine("Reves: " + res);
+    }
+}
